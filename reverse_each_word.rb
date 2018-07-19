71c8b11ca4a6b13455a641c7f6f9e6e6237c030f
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 17:13:19 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,21 @@
+
+# def reverse_each_word(str)
+#   arr = str.split(" ")
+#   new_str = ""
+#   arr.each do |word| 
+#     if word == arr.last 
+#       new_str << word.reverse
+#     else 
+#     new_str << word.reverse + " "
+#     end
+#   end
+#   new_str
+# end
+
+
 def reverse_each_word(str)
-  str.each {|word| word.reverse}
+  arr = str.split(" ")
+  new = arr.collect {|word| word.reverse}
+  new.join(" ")
 end
   
